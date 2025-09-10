89dea7a27534446b5fab52b17f9a33f54fa30eb3
karriGopichand <gopichandkarri23@gmail.com>
2025-09-10 10:21:07 +0530

    Update HelloWorld.jsx

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 const HelloWorld = () => {
   return (
     <div className="hello-container">
-      <h1 className="hello-text">ReactJS CI/CD Java Home Cloud</h1>
+      <h1 className="hello-text">Hello world Karri Gopichand</h1>
     </div>
   );
 };
 
 export default HelloWorld;
+
